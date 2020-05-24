5b14473b929149b85f6cc2a27fe98e22e4d18f01
shiri92 <shiriron92@gmail.com>
2020-05-24 09:33:08 +0300

    Updates

---
+++ precache-manifest.d29653ec3a56577d67c539ec72ae2be1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f6f42b410addf9f6118ccb446a8366",
+    "url": "/Shiri-Ron-24-05-2020/index.html"
+  },
+  {
+    "revision": "f83c46140037ea1e8914",
+    "url": "/Shiri-Ron-24-05-2020/static/css/main.8088575c.chunk.css"
+  },
+  {
+    "revision": "f1833d7c7b9cdb6a51f4",
+    "url": "/Shiri-Ron-24-05-2020/static/js/2.1f968bea.chunk.js"
+  },
+  {
+    "revision": "35a08d2e06945b779fadacb826071be3",
+    "url": "/Shiri-Ron-24-05-2020/static/js/2.1f968bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83c46140037ea1e8914",
+    "url": "/Shiri-Ron-24-05-2020/static/js/main.946f08e1.chunk.js"
+  },
+  {
+    "revision": "b643364c9ad2c4c1f27e",
+    "url": "/Shiri-Ron-24-05-2020/static/js/runtime-main.7a0cc960.js"
+  },
+  {
+    "revision": "d1a3fce6e9f4ec168f2c8285269aea07",
+    "url": "/Shiri-Ron-24-05-2020/static/media/empty.d1a3fce6.png"
+  },
+  {
+    "revision": "7dd045a8dc0e884b0a49f53abf22d2d0",
+    "url": "/Shiri-Ron-24-05-2020/static/media/full.7dd045a8.png"
+  },
+  {
+    "revision": "8c2ec30770d37b76b8cfa643f7f6122d",
+    "url": "/Shiri-Ron-24-05-2020/static/media/sun-cloud.8c2ec307.svg"
+  },
+  {
+    "revision": "1259b6d43da52a09b23caf706893e3f8",
+    "url": "/Shiri-Ron-24-05-2020/static/media/sun.1259b6d4.svg"
+  }
+]);
